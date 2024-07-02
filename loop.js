19dc2c6f988e86838ae8f22a0d53aab3d43b0d1a
Faizan Jehan <faizanpak512@gmail.com>
2024-07-02 15:50:28 +0500

    my arrow function

---
+++ loop.js
@@ -0,0 +1,40 @@
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// const changeText = () => {
+//     document.getElementById('overlayText').textContent = 'Local environment set up!';
+// };
+
+// document.getElementById('changeTextButton').addEventListener('click', changeText);
+
+// const changeText = () =>{
+//     document.getElementById('overlayText').textContent = 'This is my Local enviroment set up!';
+// };
+
+// document.getElementById('changeTextButton').addEventListener('click', changeText);
+
+
+// const changeText = () =>{
+//     document.getElementById('overlayText').textContent ='This is my Local stting!';
+// };
+// document.getElementById('changeTextButton').addEventListener('click', changeText);
+
+// const changeText = () => {
+//     const overlayText = document.getElementById('overlayText');
+//     overlayText.textContent = 'Local environment set up!';
+//     overlayText.style.color = 'yellow'; // Change the text color to yellow
+// };
+
+// document.getElementById('changeTextButton').addEventListener('click', changeText);
+
+const changeText = () =>{
+    const overlayText = document.getElementById('overlayText');
+    overlayText.textContent = 'Local Github!';
+    overlayText.style.color = 'yellow';
+}
+
+document.getElementById('changeTextButton').addEventListener('click', changeText);
+
